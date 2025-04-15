de3a8af4c10cf46bd1007ef8cd323434142f2fcf
Arian <arianrahman431@gmail.com>
2025-04-15 20:36:33 +0600

    added css ref in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SavePage from "./pages/SavePage";
 import SearchPage from "./pages/SearchPage";
-
+import './index.css'; // or whatever your CSS file is
 function App() {
   return (
     <Router>
